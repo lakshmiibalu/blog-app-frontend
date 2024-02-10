c2ffb910502b5f6bc470df4170592c2053acfade
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-10 05:44:22 +0530

    signup created

---
+++ src/components/BlogNav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BlogNav = () => {
+  return (
+    <div>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <Link class="navbar-brand" to="/">Blog App</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <Link class="nav-link active" aria-current="page" to="/">Signup</Link>
+        <Link class="nav-link" to="/signin">Signin</Link>
+        <Link class="nav-link" to="/add">Add Blog</Link>
+        <Link class="nav-link" to="/view">View Blogs</Link>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default BlogNav
